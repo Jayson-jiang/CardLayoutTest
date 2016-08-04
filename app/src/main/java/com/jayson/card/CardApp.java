fc296d556407b0f40c56408cbd2208048ce9cc00
jiangshengkai <jiangshengkai@baidu.com>
2016-08-04 16:08:25 +0800

    [master] change: add card application

---
+++ app/src/main/java/com/jayson/card/CardApp.java
@@ -0,0 +1,14 @@
+package com.jayson.card;
+
+import android.app.Application;
+
+/**
+ * Created by baidu on 16/8/4.
+ */
+public class CardApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
